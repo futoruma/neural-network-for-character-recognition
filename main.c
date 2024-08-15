e2ea2902d1ce3e51c2709a304ad0dab4aaab5e92
futoruma <122431649+futoruma@users.noreply.github.com>
2024-08-15 23:47:17 +0900

    add model with cost and forward functions

--- main.c
+++ main.c
@@ -16,22 +16,22 @@
 #define TEST_IMAGES_NUM 10000
 #define IMAGE_UNIT_LEN 784
 #define LABEL_UNIT_LEN 1
-#define IMAGE_METADATA_LEN 4
-#define LABEL_METADATA_LEN 2
+#define IMAGES_METADATA_LEN 4
+#define LABELS_METADATA_LEN 2
+#define DIGITS 10
 
-int image_metadata[IMAGE_METADATA_LEN];
-int label_metadata[LABEL_METADATA_LEN];
+int images_metadata[IMAGES_METADATA_LEN];
+int labels_metadata[LABELS_METADATA_LEN];
 
 unsigned char training_images_buffer[TRAINING_IMAGES_NUM][IMAGE_UNIT_LEN];
 unsigned char test_images_buffer[TEST_IMAGES_NUM][IMAGE_UNIT_LEN];
 unsigned char training_labels_buffer[TRAINING_IMAGES_NUM][LABEL_UNIT_LEN];
 unsigned char test_labels_buffer[TEST_IMAGES_NUM][LABEL_UNIT_LEN];
 
-float training_images_matrix[TRAINING_IMAGES_NUM][IMAGE_UNIT_LEN];
-float test_images_matrix[TEST_IMAGES_NUM][IMAGE_UNIT_LEN];
+float training_images[TRAINING_IMAGES_NUM][IMAGE_UNIT_LEN];
+float test_images[TEST_IMAGES_NUM][IMAGE_UNIT_LEN];
 int training_labels[TRAINING_IMAGES_NUM];
 int test_labels[TEST_IMAGES_NUM];
-
 
 void load_into_buffer(char *file_path, int metadata_len, int metadata[], int unit_len, int data_num, unsigned char buffer[][unit_len])
 {
@@ -50,14 +50,12 @@ void load_into_buffer(char *file_path, int metadata_len, int metadata[], int uni
   close(file_descriptor);
 }
 
-
-void save_buffer_to_images_matrix(int data_num, unsigned char buffer[][IMAGE_UNIT_LEN], float images_matrix[][IMAGE_UNIT_LEN])
+void save_buffer_to_images(int data_num, unsigned char buffer[][IMAGE_UNIT_LEN], float images[][IMAGE_UNIT_LEN])
 {
   for (size_t i = 0; i < data_num; i++)
     for (size_t j = 0; j < IMAGE_UNIT_LEN; j++)
-      images_matrix[i][j]  = (float) buffer[i][j] / 255.0f;
+      images[i][j]  = (float) buffer[i][j] / 255.0f;
 }
-
 
 void save_buffer_to_labels(int data_num, unsigned char buffer[][LABEL_UNIT_LEN], int labels[])
 {
@@ -65,41 +63,93 @@ void save_buffer_to_labels(int data_num, unsigned char buffer[][LABEL_UNIT_LEN],
     labels[i]  = (int) buffer[i][0];
 }
 
+typedef struct {
+  Matrix a0;
+  Matrix w1, b1, a1;
+  Matrix w2, b2, a2;
+  Matrix w3, b3, a3;
+} Model;
+
+void forward(Model model)
+{
+  matrix_dot(model.a1, model.a0, model.w1);
+  matrix_sum(model.a1, model.b1);
+  matrix_sig(model.a1);
+  
+  matrix_dot(model.a2, model.a1, model.w2);
+  matrix_sum(model.a2, model.b2);
+  matrix_sig(model.a2);
+
+  matrix_dot(model.a3, model.a2, model.w3);
+  matrix_sum(model.a3, model.b3);
+  matrix_softmax(model.a3);
+}
+
+float cost(Model model, int data_num, float images[][IMAGE_UNIT_LEN], int labels[])
+{
+  float cost = 0;
+  for (size_t i = 0; i < data_num; i++) {
+    
+    for (size_t j = 0; j < IMAGE_UNIT_LEN; j++) {
+      MATRIX_AT(model.a0, 0, j) = images[i][j];
+    }
+
+    forward(model);
+
+    int correct_digit = labels[i];
+    MATRIX_AT(model.a3, 0, correct_digit) -= 1.0f;
+
+    for (size_t j = 0; j < DIGITS; j++) {
+      float diff = MATRIX_AT(model.a3, 0, j);
+      cost += diff * diff;
+    }
+  }
+
+  return cost / data_num;
+}
 
 int main(void)
 {  
-  load_into_buffer(TRAINING_IMAGES_PATH, IMAGE_METADATA_LEN, image_metadata, IMAGE_UNIT_LEN, TRAINING_IMAGES_NUM, training_images_buffer);
-  save_buffer_to_images_matrix(TRAINING_IMAGES_NUM, training_images_buffer, training_images_matrix);
+  load_into_buffer(TRAINING_IMAGES_PATH, IMAGES_METADATA_LEN, images_metadata, IMAGE_UNIT_LEN, TRAINING_IMAGES_NUM, training_images_buffer);
+  save_buffer_to_images(TRAINING_IMAGES_NUM, training_images_buffer, training_images);
 
-  load_into_buffer(TEST_IMAGES_PATH, IMAGE_METADATA_LEN, image_metadata, IMAGE_UNIT_LEN, TEST_IMAGES_NUM, test_images_buffer);
-  save_buffer_to_images_matrix(TEST_IMAGES_NUM, test_images_buffer, test_images_matrix);
+  load_into_buffer(TEST_IMAGES_PATH, IMAGES_METADATA_LEN, images_metadata, IMAGE_UNIT_LEN, TEST_IMAGES_NUM, test_images_buffer);
+  save_buffer_to_images(TEST_IMAGES_NUM, test_images_buffer, test_images);
     
-  load_into_buffer(TRAINING_LABELS_PATH, LABEL_METADATA_LEN, label_metadata, LABEL_UNIT_LEN, TRAINING_IMAGES_NUM, training_labels_buffer);
+  load_into_buffer(TRAINING_LABELS_PATH, LABELS_METADATA_LEN, labels_metadata, LABEL_UNIT_LEN, TRAINING_IMAGES_NUM, training_labels_buffer);
   save_buffer_to_labels(TRAINING_IMAGES_NUM, training_labels_buffer, training_labels);
     
-  load_into_buffer(TEST_LABELS_PATH, LABEL_METADATA_LEN, label_metadata, LABEL_UNIT_LEN, TEST_IMAGES_NUM, test_labels_buffer);
+  load_into_buffer(TEST_LABELS_PATH, LABELS_METADATA_LEN, labels_metadata, LABEL_UNIT_LEN, TEST_IMAGES_NUM, test_labels_buffer);
   save_buffer_to_labels(TEST_IMAGES_NUM, test_labels_buffer, test_labels);
 
-//  for (size_t i = 0; i < 784; i++) {
-//    printf("%1.1f ", test_images_matrix[999][i]);
-//    if ( (i + 1) % 28 == 0) printf("\n");
-//  }
-//
-//  printf("label: %d\n", test_labels[999]);
-
   srand(time(0));
-  Matrix a = matrix_alloc(1, 2);
-  matrix_rand(a, 5, 10);
-  matrix_print(a, "a");
 
-  Matrix b = matrix_alloc(2, 5);
-  matrix_fill(b, 2);
-  matrix_print(b, "b");
+  Model model;
 
-  Matrix dst = matrix_alloc(1, 5);
+  model.a0 = matrix_alloc(1, 784);
 
-  matrix_dot(dst, a, b);
-  matrix_print(dst, "dst");
+  model.w1 = matrix_alloc(784, 18);
+  model.b1 = matrix_alloc(1, 18);
+  model.a1 = matrix_alloc(1, 18);
+
+  model.w2 = matrix_alloc(18, 18);
+  model.b2 = matrix_alloc(1, 18);
+  model.a2 = matrix_alloc(1, 18);
+
+  model.w3 = matrix_alloc(18, 10);
+  model.b3 = matrix_alloc(1, 10);
+  model.a3 = matrix_alloc(1, 10);
+
+  matrix_rand(model.w1, 0, 1);
+  matrix_rand(model.b1, 0, 1);
+  matrix_rand(model.w2, 0, 1);
+  matrix_rand(model.b2, 0, 1);
+  matrix_rand(model.w3, 0, 1);
+  matrix_rand(model.b3, 0, 1);
+
+  printf("cost = %f\n", cost(model, 100, training_images, training_labels));
+ 
+  MATRIX_PRINT(model.a3);
 
   return 0;
 }
