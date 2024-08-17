a0293f8afdc4ddbc0a562bc65e984de4d98a34b0
futoruma <122431649+futoruma@users.noreply.github.com>
2024-08-17 18:47:32 +0900

    fix function signatures

--- main.c
+++ main.c
@@ -19,21 +19,18 @@
 #define IMAGES_METADATA_LEN 4
 #define LABELS_METADATA_LEN 2
 #define DIGITS 10
+#define EPS 0.1f
+#define LEARNING_RATE 0.1f
 
-int images_metadata[IMAGES_METADATA_LEN];
-int labels_metadata[LABELS_METADATA_LEN];
-
-unsigned char training_images_buffer[TRAINING_IMAGES_NUM][IMAGE_UNIT_LEN];
-unsigned char test_images_buffer[TEST_IMAGES_NUM][IMAGE_UNIT_LEN];
-unsigned char training_labels_buffer[TRAINING_IMAGES_NUM][LABEL_UNIT_LEN];
-unsigned char test_labels_buffer[TEST_IMAGES_NUM][LABEL_UNIT_LEN];
+int int_buf[IMAGES_METADATA_LEN];
+unsigned char char_buf[TRAINING_IMAGES_NUM][IMAGE_UNIT_LEN];
 
 float training_images[TRAINING_IMAGES_NUM][IMAGE_UNIT_LEN];
 float test_images[TEST_IMAGES_NUM][IMAGE_UNIT_LEN];
 int training_labels[TRAINING_IMAGES_NUM];
 int test_labels[TEST_IMAGES_NUM];
 
-void load_into_buffer(char *file_path, int metadata_len, int metadata[], int unit_len, int data_num, unsigned char buffer[][unit_len])
+void load_into_buffer(char *file_path, int meta_len, int unit_len, int data_num)
 {
   int file_descriptor = open(file_path, O_RDONLY);
   if (file_descriptor == -1) {
@@ -41,26 +38,26 @@ void load_into_buffer(char *file_path, int metadata_len, int metadata[], int uni
     exit(-1);
   }
   
-  read(file_descriptor, metadata, metadata_len * sizeof(int));
+  read(file_descriptor, int_buf, meta_len * sizeof(int));
     
   for (size_t i = 0; i < data_num; i++) {
-    read(file_descriptor, buffer[i], unit_len * sizeof(unsigned char));   
+    read(file_descriptor, char_buf[i], unit_len * sizeof(unsigned char));   
   }
 
   close(file_descriptor);
 }
 
-void save_buffer_to_images(int data_num, unsigned char buffer[][IMAGE_UNIT_LEN], float images[][IMAGE_UNIT_LEN])
+void save_buffer_to_images(int data_num, float images[][IMAGE_UNIT_LEN])
 {
   for (size_t i = 0; i < data_num; i++)
     for (size_t j = 0; j < IMAGE_UNIT_LEN; j++)
-      images[i][j]  = (float) buffer[i][j] / 255.0f;
+      images[i][j]  = (float) char_buf[i][j] / 255.0f;
 }
 
-void save_buffer_to_labels(int data_num, unsigned char buffer[][LABEL_UNIT_LEN], int labels[])
+void save_buffer_to_labels(int data_num, int labels[])
 {
   for (size_t i = 0; i < data_num; i++)
-    labels[i]  = (int) buffer[i][0];
+    labels[i]  = (int) char_buf[i][0];
 }
 
 void nn_forward(NN nn)
@@ -83,10 +80,12 @@ float cost(NN nn, int data_num, float images[][IMAGE_UNIT_LEN], int labels[])
     nn_forward(nn);
 
     int correct_digit = labels[i];
-    MATRIX_AT(NN_OUTPUT(nn), 0, correct_digit) -= 1.0f;
 
     for (size_t j = 0; j < DIGITS; j++) {
       float diff = MATRIX_AT(NN_OUTPUT(nn), 0, j);
+      if (j == correct_digit) {
+        diff -= 1.0f;
+      }
       cost += diff * diff;
     }
   }
@@ -94,7 +93,7 @@ float cost(NN nn, int data_num, float images[][IMAGE_UNIT_LEN], int labels[])
   return cost / data_num;
 }
 
-void finite_diff(NN nn, NN gradient, float eps, float images[][IMAGE_UNIT_LEN], int labels[])
+void finite_diff(NN nn, NN gradient, float images[][IMAGE_UNIT_LEN], int labels[])
 {
   float saved;
   float c = cost(nn, 100, training_images, training_labels);
@@ -104,8 +103,8 @@ void finite_diff(NN nn, NN gradient, float eps, float images[][IMAGE_UNIT_LEN], 
     for (size_t i = 0; i < nn.ws[l].rows; i++) {
       for (size_t j = 0; j < nn.ws[l].cols; j++) {
         saved = MATRIX_AT(nn.ws[l], i, j);
-        MATRIX_AT(nn.ws[l], i, j) += eps;
-        MATRIX_AT(gradient.ws[l], i, j) = (cost(nn, 100, training_images, training_labels) - c) / eps; 
+        MATRIX_AT(nn.ws[l], i, j) += EPS;
+        MATRIX_AT(gradient.ws[l], i, j) = (cost(nn, 100, training_images, training_labels) - c) / EPS; 
         MATRIX_AT(nn.ws[l], i, j) = saved;
       }
     }
@@ -113,27 +112,27 @@ void finite_diff(NN nn, NN gradient, float eps, float images[][IMAGE_UNIT_LEN], 
     for (size_t i = 0; i < nn.bs[l].rows; i++) {
       for (size_t j = 0; j < nn.bs[l].cols; j++) {
         saved = MATRIX_AT(nn.bs[l], i, j);
-        MATRIX_AT(nn.bs[l], i, j) += eps;
-        MATRIX_AT(gradient.bs[l], i, j) = (cost(nn, 100, training_images, training_labels) - c) / eps; 
+        MATRIX_AT(nn.bs[l], i, j) += EPS;
+        MATRIX_AT(gradient.bs[l], i, j) = (cost(nn, 100, training_images, training_labels) - c) / EPS; 
         MATRIX_AT(nn.bs[l], i, j) = saved;
       }
     }
   }
 }
 
-void learn(NN nn, NN gradient, float rate)
+void learn(NN nn, NN gradient)
 {
   for (size_t l = 0; l < nn.count; l++) {
     
     for (size_t i = 0; i < nn.ws[l].rows; i++) {
       for (size_t j = 0; j < nn.ws[l].cols; j++) {
-        MATRIX_AT(nn.ws[l], i, j) -= rate * MATRIX_AT(gradient.ws[l], i, j);
+        MATRIX_AT(nn.ws[l], i, j) -= LEARNING_RATE * MATRIX_AT(gradient.ws[l], i, j);
       }
     }
     
     for (size_t i = 0; i < nn.bs[l].rows; i++) {
       for (size_t j = 0; j < nn.bs[l].cols; j++) {
-        MATRIX_AT(nn.bs[l], i, j) -= rate * MATRIX_AT(gradient.bs[l], i, j);
+        MATRIX_AT(nn.bs[l], i, j) -= LEARNING_RATE * MATRIX_AT(gradient.bs[l], i, j);
       }
     }
   }
@@ -141,17 +140,17 @@ void learn(NN nn, NN gradient, float rate)
 
 int main(void)
 {  
-  load_into_buffer(TRAINING_IMAGES_PATH, IMAGES_METADATA_LEN, images_metadata, IMAGE_UNIT_LEN, TRAINING_IMAGES_NUM, training_images_buffer);
-  save_buffer_to_images(TRAINING_IMAGES_NUM, training_images_buffer, training_images);
+  load_into_buffer(TRAINING_IMAGES_PATH, IMAGES_METADATA_LEN, IMAGE_UNIT_LEN, TRAINING_IMAGES_NUM);
+  save_buffer_to_images(TRAINING_IMAGES_NUM, training_images);
 
-  load_into_buffer(TEST_IMAGES_PATH, IMAGES_METADATA_LEN, images_metadata, IMAGE_UNIT_LEN, TEST_IMAGES_NUM, test_images_buffer);
-  save_buffer_to_images(TEST_IMAGES_NUM, test_images_buffer, test_images);
+  load_into_buffer(TEST_IMAGES_PATH, IMAGES_METADATA_LEN, IMAGE_UNIT_LEN, TEST_IMAGES_NUM);
+  save_buffer_to_images(TEST_IMAGES_NUM, test_images);
     
-  load_into_buffer(TRAINING_LABELS_PATH, LABELS_METADATA_LEN, labels_metadata, LABEL_UNIT_LEN, TRAINING_IMAGES_NUM, training_labels_buffer);
-  save_buffer_to_labels(TRAINING_IMAGES_NUM, training_labels_buffer, training_labels);
+  load_into_buffer(TRAINING_LABELS_PATH, LABELS_METADATA_LEN, LABEL_UNIT_LEN, TRAINING_IMAGES_NUM);
+  save_buffer_to_labels(TRAINING_IMAGES_NUM, training_labels);
     
-  load_into_buffer(TEST_LABELS_PATH, LABELS_METADATA_LEN, labels_metadata, LABEL_UNIT_LEN, TEST_IMAGES_NUM, test_labels_buffer);
-  save_buffer_to_labels(TEST_IMAGES_NUM, test_labels_buffer, test_labels);
+  load_into_buffer(TEST_LABELS_PATH, LABELS_METADATA_LEN, LABEL_UNIT_LEN, TEST_IMAGES_NUM);
+  save_buffer_to_labels(TEST_IMAGES_NUM, test_labels);
 
   srand(time(0));
 
@@ -161,13 +160,10 @@ int main(void)
 
   nn_rand(nn, 0, 1);
 
-  float eps = 0.1f;
-  float rate = 0.1f;
-
   printf("cost = %f\n", cost(nn, 100, training_images, training_labels));
-  finite_diff(nn, gradient, eps, training_images, training_labels);
+  finite_diff(nn, gradient, training_images, training_labels);
   
-  learn(nn, gradient, rate);
+  learn(nn, gradient);
   printf("cost = %f\n", cost(nn, 100, training_images, training_labels));
 
   MATRIX_PRINT(NN_OUTPUT(nn));
